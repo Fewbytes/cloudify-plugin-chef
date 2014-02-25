80d6348d11e707ddd821819366424998c77f35b1
Ilya Sher <ilya.sher@coding-knight.com>
2014-02-25 13:50:58 +0200

    Add client tests. Make tests configurable.

--- cloudify_plugin_chef/tests/test.py
+++ cloudify_plugin_chef/tests/test.py
@@ -10,20 +10,28 @@ import unittest
 
 from cloudify.mocks import MockCloudifyContext
 
+import cosmo_plugin_common as cpc
+
 import cloudify_plugin_chef.chef_client as chef_client
 import cloudify_plugin_chef.operations as chef_operations
 
 # Run on target machine!!!
 SAFETY_FILE = '/tmp/do-run-chef-tests'
-CHEF_VERSION = '11.4.4-2'
 FILE_SERVER_PORT = '50000'
 
 CHEF_CREATED_FILE_NAME = '/tmp/cloudify_plugin_chef_test.txt'
 CHEF_CREATED_FILE_CONTENTS = ''.join([random.choice(
     string.ascii_uppercase + string.digits) for x in range(6)])
 
+class TestsConfig(cpc.Config):
+    which = 'chef_tests'
 
-class ChefPluginTest(unittest.TestCase):
+os.environ.setdefault('CHEF_TESTS_CONFIG_PATH',
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chef_tests.json'))
+
+tests_config = TestsConfig().get()
+
+class ChefPluginTest(object):
 
     def setUp(self):
         if os.getuid() == 0:
@@ -43,24 +51,17 @@ class ChefPluginTest(unittest.TestCase):
         subprocess.call(['fuser', '-k', FILE_SERVER_PORT + '/tcp'])
 
     def _make_context(self, operation=None):
+        inst = self.__class__.INSTALLATION_TYPE
+        props = tests_config[inst]['properties']
+        props.setdefault('chef_attributes', {})
+        props['chef_attributes'].setdefault('create_file', {})
+        props['chef_attributes']['create_file'].setdefault('file_name', CHEF_CREATED_FILE_NAME)
+        props['chef_attributes']['create_file'].setdefault('file_contents', CHEF_CREATED_FILE_CONTENTS)
         ctx = MockCloudifyContext(
             node_id='clodufiy_app_node_id',
             operation='cloudify.interfaces.lifecycle.' +
             (operation or 'INVALID'),
-            properties={
-                'chef_version': CHEF_VERSION,
-                'chef_cookbooks': 'http://127.0.0.1:' +
-                FILE_SERVER_PORT + '/cookbooks.tar.gz',
-                'chef_attributes': {
-                    'create_file': {
-                        'file_name': CHEF_CREATED_FILE_NAME,
-                        'file_contents': CHEF_CREATED_FILE_CONTENTS,
-                    }
-                },
-                'runlists': {
-                    'install': 'recipe[create-file]'
-                }
-            }
+            properties=props
         )
         return ctx
 
@@ -68,18 +69,43 @@ class ChefPluginTest(unittest.TestCase):
         """Run Chef installation and check for expected"""
         # Assumption: chef-client version is the same
         ctx = self._make_context()
-        chef_client.chef_manager = chef_client.get_manager(ctx)
-        chef_client.chef_manager.install(ctx)
+        chef_manager = chef_client.get_manager(ctx)
+        self.assertIsInstance(chef_manager, self.__class__.CORRECT_CHEF_MANAGER)
+        chef_manager.install(ctx)
         output = subprocess.check_output(['sudo', 'chef-client', '-v'])
         m = re.match('^Chef: ([0-9.]+)', output)
-        expected_version, _, _ = CHEF_VERSION.partition('-')
+        expected_version, _, _ = tests_config['solo']['properties']['chef_version'].partition('-')
         self.assertEquals(m.group(1), expected_version)
 
     def test_chef_operation(self):
-        ctx = self._make_context('install')
+        ctx = self._make_context(operation='install')
         chef_operations.node_operation(ctx)
-        self.assertEquals(open(CHEF_CREATED_FILE_NAME).read(), CHEF_CREATED_FILE_CONTENTS)
+        create_file_attrs = ctx.properties['chef_attributes']['create_file']
+        f = create_file_attrs['file_name']
+        c = create_file_attrs['file_contents']
+        self.assertEquals(open(f).read(), c)
 
+
+class ChefPluginSoloTest(ChefPluginTest, unittest.TestCase):
+
+    INSTALLATION_TYPE = 'solo'
+    CORRECT_CHEF_MANAGER = chef_client.ChefSoloManager
+
+
+class ChefPluginClientTest(ChefPluginTest, unittest.TestCase):
+
+    INSTALLATION_TYPE = 'client'
+    CORRECT_CHEF_MANAGER = chef_client.ChefClientManager
+
+    def test_chef_installation(self):
+        super(ChefPluginClientTest, self).test_chef_installation()
+        ctx = self._make_context()
+        chef_manager = chef_client.get_manager(ctx)
+        self.assertIsInstance(chef_manager, self.__class__.CORRECT_CHEF_MANAGER)
+        try:
+            chef_manager.run(ctx, '', ctx.properties['chef_attributes'])
+        except chef_client.ChefError:
+            self.fail("Chef run failed")
 
 if __name__ == '__main__':
     unittest.main()
