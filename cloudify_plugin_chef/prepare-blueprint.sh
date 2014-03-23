c1b9165b7d2edcd57bbadb3442c5fb2461e5f0b7
Ilya Sher <ilya.sher@coding-knight.com>
2014-03-23 12:27:24 +0200

    data bags handling

--- cloudify_plugin_chef/prepare-blueprint.sh
+++ cloudify_plugin_chef/prepare-blueprint.sh
@@ -4,6 +4,8 @@ DST=test-app/plugins/cloudify-plugin-chef
 
 mkdir -p $DST
 rsync --exclude=.git --exclude='*.swp' --exclude=test-app --exclude=tests -aP ../ $DST/
-for res in environments roles cookbooks;do
+for res in environments roles cookbooks data_bags;do
+	echo "+ Packing $res"
 	tar -C tests -czf $DST/cloudify_plugin_chef/chef/$res.tar.gz $res
+	ls -l $DST/cloudify_plugin_chef/chef/$res.tar.gz | sed -e 's/^/  /'
 done
